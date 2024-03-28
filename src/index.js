9a6c5025a469b69243b1616fcdb630113b2a7c5a
Robileplubo <robdesjars@gmail.com>
2024-03-28 14:47:19 +0100

    Changement du index.html et du index.js, maintenant ça marche !

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import MovieSelector from "./RandomMovie";
 
-ReactDOM.render(<MovieSelector />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<MovieSelector />);
