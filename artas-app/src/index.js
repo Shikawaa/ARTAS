3cccfc5c9007d0f0939ae865e4a9d81ea7eab02a
alexisbaud <alexisbaud@proton.me>
2024-03-28 15:22:53 +0100

    test

--- artas-app/src/index.js
+++ artas-app/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AnswerInput from "./AnswerInput";
-import "./index.css";
 
 ReactDOM.render(<AnswerInput />, document.getElementById("root"));
