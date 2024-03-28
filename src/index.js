6794efcbf40001a96358628a45fcfec62099d8b7
Shikawa <andurandalex@gmail.com>
2024-03-28 15:32:34 +0100

    Del old files + config.json add in .gitignore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Test from "./Test";
-import Main from "./Main";
-import "./index.css";
-
-ReactDOM.render(<Test />, document.getElementById("root"));
-
